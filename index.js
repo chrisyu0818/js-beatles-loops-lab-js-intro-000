439bfa3e29522740875eeee6f8ce579d13bc9952
Zhenhua yu <zyu0628@gmail.com>
2020-10-12 15:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
+  var array = []
   for (let i=0; i<musicians.length; i++) {
-    array.push(`${musicians[i]} + "plays" + ${instruments[i]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
 }
